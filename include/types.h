3044e7f274f591ba13fdc519775526aa1759a4e6
Guillaume <saby.guillaume@gmail.com>
2014-04-06 17:33:07 +0100

    Added a type definition. This has to be redefine for all palteform etc...

---
+++ include/types.h
@@ -0,0 +1,24 @@
+#ifndef _ALFAR_TYPES_H
+#define _ALFAR_TYPES_H
+
+#if _WIN64
+
+#include <stdint.h>
+
+#define TRUE	1
+#define FALSE	0
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+
+typedef float float32;
+typedef double float64;
+
+#endif
+
+#endif
